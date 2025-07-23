5e9da769e4367f53331a4c5760338fb7ae1ab5be
unknown <s.livvan.k+busyCookie@gmail.com>
2025-07-23 13:58:14 +0300

    Test updated to verify presence of all required elements of starting page of the service.

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.*;
 //Selenide
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 
 /**
  *
@@ -21,9 +22,43 @@ public class MainPageTest {
     public void openTransportTallinn() {
         open("https://transport.tallinn.ee//");
         
+        //check that page is loaded
         $(".MapDisplayed.home").should(exist);
         $(".MapDisplayed.home").shouldBe(visible);
-
+        
+        //check that page contains correct basic elements
+        //Header with lanugae selction menu
+        $("#divHeader").should(exist);
+        $("#divHeader").shouldBe(visible);
+        
+        $("#divTitle").should(exist);
+        $("#divTitle").shouldBe(visible);
+        $("#divTitle").shouldHave(text("Avaleht"));
+        
+        $("#divHeader").find(byId("divLang")).should(exist);
+        $("#divHeader").find(byId("divLang")).shouldBe(visible);
+        
+        //Sidebar menus
+        $("#sidebar").should(exist);
+        $("#sidebar").shouldBe(visible);
+        
+        $("#sidebar").find(byId("areaMenuContainer")).should(exist);
+        $("#sidebar").find(byId("areaMenuContainer")).shouldBe(visible);
+        
+        $("#sidebar").find(byId("mainMenu")).should(exist);
+        $("#sidebar").find(byId("mainMenu")).shouldBe(visible);
+        
+        $("#sidebar").find(byId("infoMenu")).should(exist);
+        $("#sidebar").find(byId("infoMenu")).shouldBe(visible);
+        
+        //Travel planner
+        $("#divContentIndex.travel-planner").should(exist);
+        $("#divContentIndex.travel-planner").shouldBe(visible);
+        
+        //Map
+        $("#divMapWrapper").should(exist);
+        $("#divMapWrapper").shouldBe(visible);
+        
     }
     
 }
